70f6c96c8076b88b5f89432fe9612f2bb7415f9a
tuanvision <tuannm0312@gmail.com>
2020-09-17 13:06:54 +0700

    add drawer navigation

---
+++ app/src/main/java/com/example/DrawerNavigation/ui/slideshow/SlideshowFragment.kt
@@ -0,0 +1,29 @@
+package com.example.DrawerNavigation.ui.slideshow
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.example.testopencv.R
+
+class SlideshowFragment : Fragment() {
+
+    private val slideshowViewModel: SlideshowViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
+        val textView: TextView = root.findViewById(R.id.text_slideshow)
+        slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
